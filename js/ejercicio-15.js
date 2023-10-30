09f3988867c8f8373aa66435566715821e4fd5ba
Martin Cornejo <martincckd130@gmail.com>
2023-10-30 01:25:36 -0300

    Ejercicio 15 finalizado

---
+++ js/ejercicio-15.js
@@ -0,0 +1,21 @@
+// EJERCICIO 15 
+// 15- Realiza un script que cuente el número de vocales que tiene un texto
+
+let frase = prompt("Escriba una palabra o frase y nombraremos el total de vocales que contenga");
+let vocales = 0;
+
+if (frase === null) {
+    alert('Cancelaste el programa'); 
+} 
+else {
+    for (let i = 0; i < frase.length; i++) {
+        let caracteres = frase[i];
+    
+        if (caracteres == 'a' || caracteres == 'e' || caracteres == 'i' || caracteres == 'o' || caracteres == 'u') {
+            vocales++;
+        }
+    }
+    
+    document.write(`<p>Texto: ${frase} <br>Número total de vocales: ${vocales}</p>`);
+}
+
