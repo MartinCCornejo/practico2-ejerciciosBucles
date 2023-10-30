43718b25b0951dce19b73fa4f1cf105d3588ff21
Martin Cornejo <martincckd130@gmail.com>
2023-10-30 16:14:17 -0300

    Ejercicio 11 finalizado

--- js/ejercicio-11.js
+++ js/ejercicio-11.js
@@ -3,20 +3,33 @@
 
 // Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp
 
-let nombre = '';
-let edad = '';
 
-for (let i = 1; i <= 3; i++) {
-    let nuevoNombre = prompt('Ingresa un nombre');
-    let nuevaEdad = prompt(`Ingrese la edad de ${nuevoNombre}`);
+let nombre1 = prompt('Escriba el nombre de la primera persona');
+let edad1 = parseInt(prompt(`Escriba la edad de ${nombre1}`));
 
-    edad+= nuevaEdad +',';
-    nombre+= nuevoNombre +',';
+let nombre2 = prompt('Escriba el nombre de la segunda persona');
+let edad2 = parseInt(prompt(`Escriba la edad de ${nombre2}`));
+
+let nombre3 = prompt('Escriba el nombre de la tercera persona');
+let edad3 = parseInt(prompt(`Escriba la edad de ${nombre3}`));
+
+let edadMayor = Math.max(edad1,edad2,edad3);
+let nombreMayor = "";
+
+
+if (edad1 > edad2 && edad1 > edad3) {
+    nombreMayor+= nombre1;
+} else if(edad2 > edad1 && edad2 > edad3) {
+    nombreMayor+= nombre2;
+} else if (edad3 > edad1 && edad3 > edad2) {
+    nombreMayor+= nombre3;
+} else{
+    alert('Por favor indique una edad válida');
 }
 
-edad = edad.slice(0,-1)
-nombre = nombre.slice(0,-1)
-let edades = edad.split(',');
-let nombres = nombre.split(',');
-let edadMayor = Math.max(edades[0],edades[1],edades[2]);
 
+if (nombreMayor === null || nombreMayor.length === 0 || isNaN(edadMayor)) {
+    alert('Datos incorrectos. Por favor, verifique los nombres o la edad de las personas');
+} else{
+    alert(`La persona más grande es ${nombreMayor} con ${edadMayor} años.`);
+}
